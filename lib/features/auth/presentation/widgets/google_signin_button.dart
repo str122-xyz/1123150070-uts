af89a4fc782d0caa9786944a28f2b68197adc451
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-17 17:54:06 +0700

    feat: tambah file baru dan setup awal

---
+++ lib/features/auth/presentation/widgets/google_signin_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final bool isLoading;
+
+  const GoogleSignInButton({super.key, this.onPressed, this.isLoading = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 52,
+      child: OutlinedButton(
+        onPressed: isLoading ? null : onPressed,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.white,
+          side: BorderSide(color: Colors.grey.shade300),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: isLoading,
+      ),
+    );
+  }
+}
