f0b8ec27bd91709825d227e6ddbbaf5427b8c0a5
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-17 17:19:38 +0700

    feat: tambah file baru dan setup awal custom txt field

---
+++ lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/constants/app_colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final TextEditingController controller;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final bool enabled;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    required this.hint,
+    required this.controller,
+    this.obscureText = false,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.enabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
