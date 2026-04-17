b7e2993561af076e480436b8f165d8bf78e7c3c2
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-17 20:40:08 +0700

    buat file baru, import an, setup awal

---
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import '../../../../core/services/service_storage.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../../data/repositories/auth_repository_impl.dart';
