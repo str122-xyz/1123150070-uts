99da344ff83eb6eb204fa39b9ca694cb1a2261b0
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-23 05:25:51 +0700

    feat: halaman dashboard
    konfigurasi dan setup awal.

---
+++ lib/features/catalog/presentation/pages/dashboard_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../../../auth/presentation/providers/auth_provider.dart';
+import '../../data/models/product_model.dart';
+import '../providers/catalog_provider.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    final catalog = context.watch<CatalogProvider>();
+    final user = context.watch<AuthProvider>().firebaseUser;
+  }
+}
