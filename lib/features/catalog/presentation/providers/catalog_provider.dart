ead058124ffd2a5b96ed68681f00345a7c0892ac
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-23 05:11:53 +0700

    feat: provider katalog data dummy
    Menambahkan CatalogProvider untuk mengelola state daftar produk kopi dan makanan, memenuhi syarat penggunaan Provider pada soal.

--- lib/features/catalog/presentation/providers/catalog_provider.dart
+++ lib/features/catalog/presentation/providers/catalog_provider.dart
@@ -23,5 +23,67 @@ class CatalogProvider extends ChangeNotifier {
 
     // Simulasi loading jaringan 1.5 detik
     await Future.delayed(const Duration(milliseconds: 1500));
+
+    _products = [
+      //kategori minuman kopi
+      const ProductModel(
+        id: 'c1',
+        name: 'Kopi Susu Ngopss',
+        description: 'Signature espresso dengan susu segar dan gula aren asli.',
+        price: 25000,
+        imageUrl:
+            'https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?q=80&w=500&auto=format&fit=crop',
+        category: 'kopi',
+      ),
+      const ProductModel(
+        id: 'c2',
+        name: 'Caffe Latte',
+        description:
+            'Kombinasi sempurna espresso dan steamed milk yang lembut.',
+        price: 28000,
+        imageUrl:
+            'https://images.unsplash.com/photo-1572442388796-11668a67e53d?q=80&w=500&auto=format&fit=crop',
+        category: 'kopi',
+      ),
+      const ProductModel(
+        id: 'c3',
+        name: 'Americano',
+        description: 'Definisi dari pahitnya kehidupan.',
+        price: 20000,
+        imageUrl:
+            'https://images.unsplash.com/photo-1551030173-122aabc4489c?q=80&w=500&auto=format&fit=crop',
+        category: 'kopi',
+      ),
+
+      //kategori makanan
+      const ProductModel(
+        id: 'f1',
+        name: 'Butter Croissant',
+        description:
+            'Pastry Prancis klasik yang renyah di luar dan lembut di dalam.',
+        price: 22000,
+        imageUrl:
+            'https://images.unsplash.com/photo-1549903072-7e6e0ebc9056?q=80&w=500&auto=format&fit=crop',
+        category: 'makanan',
+      ),
+      const ProductModel(
+        id: 'f2',
+        name: 'Choco Brownie',
+        description: 'Fudgy brownie padat dengan potongan dark chocolate.',
+        price: 18000,
+        imageUrl:
+            'https://images.unsplash.com/photo-1606313564200-e75d5e30476c?q=80&w=500&auto=format&fit=crop',
+        category: 'makanan',
+      ),
+      const ProductModel(
+        id: 'f3',
+        name: 'Pisang Goreng',
+        description: 'Pisang goreng enak muanis.',
+        price: 15000,
+        imageUrl:
+            'https://images.unsplash.com/photo-1762941904142-9d91ca413e66?q=80&w=735&auto=format&fit=crop',
+        category: 'makanan',
+      ),
+    ];
   }
 }
