7a45aaf1805a5a8bd03345d05e1ead63adb7bd58
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-23 01:46:12 +0700

    feat: splash screen dan logic rute awal
    untuk mengecek status onboarding dan autentikasi token, lalu mengarahkan user ke halaman yang tepat.

---
+++ lib/features/auth/presentation/pages/splash_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/routes/app_router.dart';
+import '../../../../core/services/service_storage.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _checkInitialRoute();
+  }
+
+  Future<void> _checkInitialRoute() async {
+    //Kasih delay 2 detik
+    await Future.delayed(const Duration(seconds: 2));
+    if (!mounted) return;
+
+    final hasSeenOnboarding = await SecureStorageService.hasSeenOnboarding();
+    final token = await SecureStorageService.getToken();
+
+    if (!hasSeenOnboarding) {
+      //Baru first pertama kali install
+      Navigator.pushReplacementNamed(context, AppRouter.onboarding);
+    } else if (token != null) {
+      //Udah pernah install dan token login masih ada
+      Navigator.pushReplacementNamed(context, AppRouter.dashboard);
+    } else {
+      //Udah pernah install tapi belum login / abis logout
+      Navigator.pushReplacementNamed(context, AppRouter.login);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.coffee_rounded, size: 80, color: Colors.white),
+            SizedBox(height: 16),
+            Text(
+              'Ngopss',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
