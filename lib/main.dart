77652931a6efd9c2f0388194f1593b3dafbacbe3
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-23 02:05:43 +0700

    feat: inisialisasi main entry point aplikasi
    setup main.dart dengan konfigurasi Firebase, MultiProvider untuk AuthProvider, dan mengatur rute awal ke SplashPage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'core/theme/app_theme.dart';
+import 'core/routes/app_router.dart';
+import 'features/auth/presentation/providers/auth_provider.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        //Todo: product provider here.
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ngopss',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      initialRoute: AppRouter.splash,
+      routes: AppRouter.routes,
+    );
+  }
+}
