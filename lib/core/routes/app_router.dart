66a3e335f339d937abdcbb6aa40be4636238dbfd
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-23 21:43:52 +0700

    refactor: add route cart
    mendaftarkan CartPage ke dalam Named Routes untuk memusatkan manajemen navigasi aplikasi.

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -4,6 +4,7 @@ import '../../features/auth/presentation/pages/register_page.dart';
 import '../../features/auth/presentation/pages/verify_email_page.dart';
 import '../../features/auth/presentation/pages/onboarding_page.dart';
 import '../../features/auth/presentation/pages/splash_page.dart';
+import '../../features/cart/presentation/pages/cart_page.dart';
 import 'package:ngopss/features/catalog/presentation/pages/dashboard_page.dart';
 
 class AppRouter {
@@ -13,6 +14,7 @@ class AppRouter {
   static const String register = '/register';
   static const String verifyEmail = '/verify-email';
   static const String dashboard = '/dashboard';
+  static const String cart = '/cart';
 
   static Map<String, WidgetBuilder> get routes => {
     splash: (_) => const SplashPage(),
@@ -21,5 +23,6 @@ class AppRouter {
     register: (_) => const RegisterPage(),
     verifyEmail: (_) => const VerifyEmailPage(),
     dashboard: (_) => const DashboardPage(),
+    cart: (_) => const CartPage(),
   };
 }
