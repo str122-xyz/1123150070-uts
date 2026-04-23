4448e36557186bff24bb0ad848af8a6b6d60e36c
Satriahrl_ <satriaherlambang22@gmail.com>
2026-04-23 20:40:12 +0700

    feat: cart page
    setup awal

---
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../providers/cart_provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = context.watch<CartProvider>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Keranjang Ngops',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: AppColors.textPrimary,
+        elevation: 0,
+      ),
+    );
+  }
+}
