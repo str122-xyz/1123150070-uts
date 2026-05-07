c7dc4abd85876f843693eb62a848e9f9a3798571
Satriahrl_ <satriaherlambang22@gmail.com>
2026-05-07 22:35:33 +0700

    feat: theme mode
    memperbarui AppTheme dengan racikan warna gelap khusus aplikasi.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../constants/app_colors.dart';
 
 class AppTheme {
+  //light mode
   static ThemeData get light {
     return ThemeData(
       useMaterial3: true,
@@ -97,6 +98,121 @@ class AppTheme {
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          textStyle: const TextStyle(
+            fontFamily: 'Inter',
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+          elevation: 2,
+        ),
+      ),
+    );
+  }
+
+  //dark mode
+  static ThemeData get dark {
+    return ThemeData(
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.dark,
+
+      // Kustomisasi Font
+      textTheme: const TextTheme(
+        // untuk Judul/Heading (Title)
+        displayLarge: TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFFEEEEEE),
+        ),
+        titleLarge: TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+          color: Color(0xFFEEEEEE),
+        ),
+        titleMedium: TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: Color(0xFFEEEEEE),
+        ),
+
+        // untuk Isi/Deskripsi Panjang (Body)
+        bodyLarge: TextStyle(
+          fontFamily: 'Lora',
+          fontSize: 16,
+          color: Color(0xFFE0E0E0),
+          height: 1.5,
+        ),
+        bodyMedium: TextStyle(
+          fontFamily: 'Lora',
+          fontSize: 14,
+          color: Color(0xFFBDBDBD),
+          height: 1.5,
+        ),
+
+        // untuk Teks Fungsional/Kecil (Caption, Harga, Kategori)
+        labelLarge: TextStyle(
+          fontFamily: 'Inter',
+          fontSize: 14,
+          fontWeight: FontWeight.w600,
+          color: Color(0xFF8D6E63),
+        ),
+        labelSmall: TextStyle(
+          fontFamily: 'Inter',
+          fontSize: 11,
+          color: Color(0xFF9E9E9E),
+        ),
+      ),
+
+      colorScheme: const ColorScheme.dark(
+        primary: Color(0xFF8D6E63), // Primary color
+        surface: Color(0xFF1E1E1E), // Permukaan Card/Navbar
+        background: Color(0xFF121212), // Background paling belakang
+        error: Color(0xFFCF6679),
+      ),
+      scaffoldBackgroundColor: const Color(0xFF121212),
+
+      // Form Input
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: const Color(0xFF1E1E1E),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFF3A3A3A)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFF3A3A3A)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFF8D6E63), width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFFCF6679)),
+        ),
+        hintStyle: const TextStyle(
+          fontFamily: 'Inter',
+          fontSize: 13,
+          color: Color(0xFF757575),
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+      ),
+
+      // button
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF8D6E63),
           foregroundColor: Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
